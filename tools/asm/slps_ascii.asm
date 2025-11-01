f769d403c727c28416a3bdd7ae6421601a4eec09
fortiersteven <fortiersteven1@gmail.com>
2025-11-01 12:40:24 -0400

    Hacking for MAP text and translation

--- tools/asm/slps_ascii.asm
+++ tools/asm/slps_ascii.asm
@@ -283,6 +283,16 @@ LAB_00390290:
 	j 		0x3902A8
 	
 	
+;ao: param used for texture id
+;a1: param used for text address but we can reuse it 
+LAB_Adjust_PB:
+	lh		$v1, 0xa0($s4)
+	addiu 	$v1, $v1, -0x3
+	sh     	$v1, 0xa0($s4)
+	jal 	0x1A0980
+	nop 
+	j 	 	0x390468
+		
 	
 ; Adjust Read_String2 to go over 0x3F thing
 ; t0 = 0x34
