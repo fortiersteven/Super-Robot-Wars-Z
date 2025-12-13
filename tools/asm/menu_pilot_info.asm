54a2458d9e13e1381ab98532a2266c1048e818ff
fortiersteven <fortiersteven1@gmail.com>
2025-12-13 17:16:27 -0500

    First hacking pass

--- tools/asm/menu_pilot_info.asm
+++ tools/asm/menu_pilot_info.asm
@@ -1,27 +1,33 @@
 .ps2
 .open __SLPS_PATH__, 0x00FE580
 
+; Stats CQB / SKL sizes 
 .org 0x0038DA40
 	li 	$a0, 0x13
 	
-.org 0x0038D9A4
-	li 	$a0, 0x13
+; CQB / SKL Value size
+.org 0x0038DB2C
+	li	$a0, 0x13
 	
+; Stats / Captain Effect size
+.org 0x0038D9A4
+	li 	$a0, 0x0C
+	
+; LV / Will / SP Label size
 .org 0x0038D644
 	li	$a0, 0x0C
 	
-;Left Stats value
+; LV / Will / SP Value size 
 .org 0x0038D7E0
 	li	$a0, 0x0C
 	
+; Next Level Label size
 .org 0x0038D728
 	li	$a0, 0x0C
 	
+; Pilot name size
 .org 0x0038D758
 	li	$a0, 0x0C
-	
-.org 0x0038D9A4	
-	li	$a0, 0x13
 	
 	
 ;Ability Label positions
@@ -34,8 +40,7 @@
 .org 0x0038DA40
 	li	$a0, 0x13
 	
-.org 0x0038DB2C
-	li	$a0, 0x13
+
 	
 ;Terrain type
 .org 0x0038DDC4
@@ -48,13 +53,15 @@
 .org 0x0038DE50
 	li	$s4, 0x4B
 	
-;Label Skills
+; Skill Label Size
 .org 0x0038DE9C
 	li 	$a0, 0x0C
 	
+; Skill Label X position
 .org 0x0038DEC0
-	li	$a2, -0xEE
-;Skill Values
+	li	$a2, -0xEA
+	
+; Skill Label Values
 .org 0x0038DEDC
 	li 	$a0, 0x13
 	
@@ -65,12 +72,22 @@
 .org 0x0038DFA8
 	li 	$a0, 0x0C
 	
-;Spirit Values
+;Spirit Command Size
 .org 0x0038DFE8
 	li 	$a0, 0x13
 	
+; Spirit Command Adjust (  ) X Position
+.org 0x38E090
+	li 	$a1, -0xC
+	
+; Spirit Command Adjust Y Position
 .org 0x0038DFF8
 	li 	$s3, 0x16
+	
+; Skills Adjust Y Position 
+
+.org 0x38DEF0
+	li 	$s4, 0x16
 ;.org 0x0038D6A0
 ;	li a3, -0x28
 
